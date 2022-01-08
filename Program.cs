41f962b3bc4a0e9db4fc7d9a2baa08cb18be14f9
aymans99 <76987646+aymans99@users.noreply.github.com>
2022-01-08 14:55:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace FibonacciSeries
+{
+    class Program
+    {
+        public int Fibo(int n)
+        {
+            int fn = 0, sn = 1, result = 0;
+
+            if (n == 0) return 0; //To return the first Fibonacci number   
+            if (n == 1) return 1; //To return the second Fibonacci number   
+
+
+            for (int i = 2; i <= n; i++)
+            {
+                result = fn + sn;
+                fn = sn;
+                sn = result;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the Fibonacci Series: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", Fibo(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fiboTest
+{
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+    
+        public void TestMethod1()
+        {
+            Program p = new Program();
+            double res = p.Fibo(8);
+            Assert.AreEqual(res, "0 1 1 2 3 5 8 13");
+        }
+    
+    }
+}
